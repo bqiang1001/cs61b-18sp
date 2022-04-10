b4fe1a53af8e20d954f7a8b4ca89f92c2c7e1512
cent <numberisflying@gmail.com>
2022-04-10 17:39:03 +0800

    -Proj1b, v2

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,113 @@
+public class ArrayDeque<T> implements Deque<T> {
+    private int capacity = 8;
+    private final double fillingFactor = 0.25;
+    private int front;
+    private int size;
+    private T[] arr;
+    public ArrayDeque() {
+        arr = (T[]) new Object[capacity];
+        front = 0;
+        size = 0;
+    }
+
+//    public ArrayDeque(ArrayDeque other) {
+//        arr = (T[]) new Object[other.size()];
+//        front = 0;
+//        back = 0;
+//        for (int i = 0; i < other.size(); i++) {
+//            this.addLast((T) other.get(i));
+//        }
+//        size = other.size();
+//        capacity = other.capacity;
+//    }
+
+    private void resize(int newCapacity) {
+        T[] newArray = (T[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = arr[(front + i) % capacity];
+        }
+        front = 0;
+        arr = newArray;
+        capacity = newCapacity;
+    }
+    @Override
+    public void addFirst(T item) {
+        if (size == capacity) {
+            resize(2 * capacity);
+        }
+        if (front == 0) {
+            front = capacity - 1;
+        } else {
+            front--;
+        }
+        arr[front] = item;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        if (size == capacity) {
+            resize(2 * capacity);
+        }
+        int back = (front + size) % capacity;
+        arr[back] = item;
+        size++;
+    }
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    @Override
+    public int size() {
+        return size;
+    }
+    @Override
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            int idx = (front + i) % capacity;
+            System.out.print(arr[idx]);
+            if (i != size - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.print('\n');
+    }
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        if (size * 4 < capacity && capacity >= 16) {
+            resize(capacity / 4);
+        }
+
+        T returnedItem = arr[front];
+        arr[front] = null;
+        front = (front + 1) % capacity;
+        size--;
+        return returnedItem;
+    }
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        if (size * 4 < capacity && capacity >= 16) {
+            resize(capacity / 4);
+        }
+        int back = (front + size - 1) % capacity;
+        T returnedItem = arr[back];
+        arr[back] = null;
+        back = back == 0 ? capacity - 1 : back - 1;
+        size--;
+        return returnedItem;
+    }
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        int idx = (front + index) % capacity;
+        return arr[idx];
+    }
+}
---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,133 @@
+public class LinkedListDeque<T> implements Deque<T>{
+
+
+    private static class ListNode<T> {
+        private T item;
+        private ListNode next;
+        private ListNode prev;
+        public ListNode(T val) {
+            item = val;
+            next = null;
+            prev = null;
+        }
+
+        public ListNode() {
+            item = null;
+            next = null;
+            prev = null;
+        }
+    }
+
+    private int size;
+    private ListNode sentinel;
+
+    public LinkedListDeque() {
+        sentinel = new ListNode();
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+//    public LinkedListDeque(LinkedListDeque other) {
+//        sentinel = new ListNode();
+//        sentinel.next = sentinel;
+//        sentinel.prev = sentinel;
+//        size = 0;
+//        for (int i = 0; i < other.size(); i++) {
+//            this.addLast((T) other.get(i));
+//        }
+//    }
+
+    public void addFirst(T item) {
+        ListNode temp = sentinel.next;
+        sentinel.next = new ListNode(item);
+        sentinel.next.next = temp;
+        temp.prev = sentinel.next;
+        sentinel.next.prev = sentinel;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        ListNode temp = sentinel.prev;
+        temp.next = new ListNode(item);
+        temp.next.next = sentinel;
+        sentinel.prev = temp.next;
+        temp.next.prev = temp;
+        size++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        int k = size;
+        ListNode node = sentinel;
+        while (k-- > 0) {
+            System.out.print(node.item);
+            node = node.next;
+            if (k > 0) {
+                System.out.print(" ");
+            }
+        }
+        System.out.print("\n");
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        ListNode returnNode = sentinel.next;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return (T) returnNode.item;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        ListNode returnNode = sentinel.prev;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return (T) returnNode.item;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        ListNode node = sentinel.next;
+        while (index-- > 0) {
+            node = node.next;
+        }
+        return (T) node.item;
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return getRecursive(index, sentinel.next);
+    }
+
+    private T getRecursive(int index, ListNode node) {
+        if (index == 0) {
+            return (T) node.item;
+        }
+        return getRecursive(index - 1, node.next);
+    }
+}
